604aa88e423437e97c9fc421ebd451f2e1d69dc9
Arturo Barba <arturo.barba@iesdonana.org>
2018-12-04 21:04:42 +0100

    Ordenamos géneros alfabeticamente

--- controllers/GenerosController.php
+++ controllers/GenerosController.php
@@ -14,7 +14,7 @@ class GenerosController extends \yii\web\Controller
     public function actionIndex()
     {
         $filas = \Yii::$app->db
-            ->createCommand('SELECT * FROM generos')->queryAll();
+            ->createCommand('SELECT * FROM generos ORDER BY genero')->queryAll();
         return $this->render('index', [
             'filas' => $filas,
         ]);
